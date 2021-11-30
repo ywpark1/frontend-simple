aa963a5cdebd4d82eece048d3b26df8d105f83ce
Yeon Woo Park <ywoo1234@gmail.com>
2021-11-29 20:31:15 -0500

    Retrieve data from server request(/v1/movies)

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,26 +5,53 @@ import { Link } from "react-router-dom";
 export default class Movies extends Component {
   state = {
     movies: [],
+    isLoaded: false,
+    error: null,
   };
 
   componentDidMount() {
-    console.log("Movies Rendered");
-    this.setState({
-      movies: [
-        { id: 1, title: "Inception", runtime: 142 },
-        { id: 2, title: "Hello", runtime: 120 },
-        { id: 3, title: "Avatar", runtime: 112 },
-      ],
-    });
+    fetch("http://localhost:4000/v1/movies")
+      .then((res) => {
+        if (res.status !== "200") {
+          let err = Error;
+          err.message = "Invalid response code: " + res.status;
+          this.setState({ error: err });
+        }
+        return res.json();
+      })
+      .then((json) => {
+        this.setState(
+          {
+            movies: json.movies,
+            isLoaded: true,
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            });
+          }
+        );
+      });
   }
 
   render() {
+    const { movies, isLoaded, error } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message} </div>;
+    }
+
+    if (!isLoaded) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <Fragment>
         <h2>Choose a Movie</h2>
 
         <ul>
-          {this.state.movies.map((m) => (
+          {movies.map((m) => (
             <li key={m.id}>
               <Link to={`/movies/${m.id}`}>{m.title}</Link>
             </li>
